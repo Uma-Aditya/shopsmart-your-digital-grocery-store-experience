61211d098e5522c776e1992e26950201b42f4923
Uma-Aditya <mohanaditya706@gmail.com>
2025-07-23 07:52:09 +0530

    Refactored project structure: moved server to backend and client to frontend

--- client/src/components/Input.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Input.css';
-
-const Input = ({ className = '', ...props }) => (
-  <input className={`ui-input ${className}`} {...props} />
-);
-
-export default Input; 
